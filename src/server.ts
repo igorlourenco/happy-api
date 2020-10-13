42cde3dc1cc99af3cfec12ba24a1f2ec76c51c61
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-13 17:53:21 -0300

    Finish initial setup

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 const app = express();
 
+// rota = tudo
+// recurso = usuario
+// metodo http = get, post, put, delete
+// parametros =
+
+app.get('/users', (request, response) => {
+    return response.json({"message": "hello"})
+})
+
 app.listen(3333)
