519c60a70c68ce8bb2d003f361601dc2ae71a468
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-13 20:54:22 -0300

    Add function to create a new orphanage

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-
 import './database/connection';
+import {getRepository} from 'typeorm';
+import Orphanage from "./models/Orphanage";
 
 const app = express();
 app.use(express.json());
@@ -10,8 +11,32 @@ app.use(express.json());
 // metodo http = get, post, put, delete
 // parametros =
 
-app.get('/users', (request, response) => {
-    return response.json({"message": "hello"})
+app.post('/orphanages', async (request, response) => {
+    const{
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.json({"success": "ok"})
 })
 
 app.listen(3333)
