9d354c8b13632fd77424974d01ae9218d522ca89
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-13 21:09:13 -0300

    Get one orphanage

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -37,5 +37,15 @@ export default {
         await orphanagesRepository.save(orphanage);
 
         return response.status(201).json(orphanage);
-    }
+    },
+
+    async show(request: Request, response: Response) {
+        const {id} = request.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
+    },
 }
