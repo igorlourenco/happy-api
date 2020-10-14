e47f788f233d3916839a3d466fac3baab8344e12
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-14 18:33:43 -0300

    Finish settinfs

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,9 @@ import errorHandler from "./errors/handler";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
-app.use(errorHandler)
-app.use(cors())
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
